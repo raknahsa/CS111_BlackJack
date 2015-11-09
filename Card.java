e9ed3841f19874484bc9e9ae5e57cdb8da48b5af
Raknahsa <raknahsa@gmail.com>
2015-11-08 21:14:47 -0500

    Change Variables
    Change the variables into numerical values.

--- Card.java
+++ Card.java
@@ -132,31 +132,31 @@ public class Card
 	public int getValue()
 	{
 		switch(this.face){
-			case 1: return ACE;
+			case 1: return 1;
 		//		break;
-			case 2: return TWO;
+			case 2: return 2;
 		//		break;
-			case 3: return THREE;
+			case 3: return 3;
 		//		break;
-			case 4: return FOUR;
+			case 4: return 4;
 		//		break;
-			case 5: return FIVE;
+			case 5: return 5;
 		//		break;
-			case 6: return SIX;
+			case 6: return 6;
 		//		break;
-			case 7: return SEVEN;
+			case 7: return 7;
 		//		break;
-			case 8: return EIGHT;
+			case 8: return 8;
 		//		break;
-			case 9: return NINE;
+			case 9: return 9;
 		//		break;
-			case 10: return TEN;
+			case 10: return 10;
 		//		break;
-			case 11: return TEN; //Jack returns 10
+			case 11: return 10; //Jack returns 10
 	  //		break;
-			case 12: return TEN;//Queen returns 10
+			case 12: return 10;//Queen returns 10
 		//		break;
-			case 13: return TEN;//King returns 10
+			case 13: return 10;//King returns 10
 		//		break;
 			default: return -1; //Should never happen
 		// 		break;
